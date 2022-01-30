75b90e576ce9f1d76fbc6d05c78fc866508a3b93
Mariem-Hamraoui <mariamhamraoui@gmail.com>
2022-01-30 16:26:01 +0100

    record inserted

--- controllers.js
+++ controllers.js
@@ -2,85 +2,77 @@ const db = require("./mimdb");
 const project = db.projects;
 const Op = db.Sequelize.Op;
 
-exports.create = (req, res) => {
-       // Create a project
-    const project = {
-      title: req.body.title,
-      note: req.body.note,   
-      status: req.body.status,
-      solo: req.body.solo ? req.body.solo : false,  
-    }; 
-    // Save project in the database
-    project.create(project)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the project."
-        });
+exports.create = (req, res) => { 
+  // Save project in the database
+  project.create(project)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the project."
       });
-  };
+    });
+};
 
 exports.findAll = (req, res) => {
-    const title = req.query.title;
-    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-  
-    project.findAll({ where: condition })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving projects."
-        });
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  project.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving projects."
       });
-  };
+    });
+};
 
 exports.findOne = (req, res) => {
-    const id = req.params.id; 
-    project.findByPk(id)
-      .then(data => {
-        if (data) {
-          res.send(data);
-        } else {
-          res.status(404).send({
-            message: `Cannot find project with id=${id}.`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message : "Error retrieving project with id=" + id
+  const id = req.params.id;
+  project.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find project with id=${id}.`
         });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving project with id=" + id
       });
-  };
+    });
+};
 
 exports.update = (req, res) => {
-    const id = req.params.id;
-  
-    project.update(req.body, {
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "successfully updated ."
-          });
-        } else {
-          res.send({
-            message: `Cannot update this project id=${id} `
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating project with id=" + id
+  const id = req.params.id;
+
+  project.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "successfully updated ."
         });
+      } else {
+        res.send({
+          message: `Cannot update this project id=${id} `
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating project with id=" + id
       });
-  };
+    });
+};
 
 exports.delete = (req, res) => {
   const id = req.params.id;
@@ -109,32 +101,32 @@ exports.delete = (req, res) => {
 
 
 exports.deleteAll = (req, res) => {
-    project.destroy({
-      where: {},
-      truncate: false
+  project.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} projects were deleted successfully!` });
     })
-      .then(nums => {
-        res.send({ message: `${nums} projects were deleted successfully!` });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all projects."
-        });
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all projects."
       });
-  };
+    });
+};
 
 
 exports.findAllsolo = (req, res) => {
-    project.findAll({ where: { solo: true } })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving projects."
-        });
+  project.findAll({ where: { solo: true } })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving projects."
       });
-  };
+    });
+};
 
