2043a72a68352fe095c5cd18761fd33ed20b48a7
Mariem-Hamraoui <mariamhamraoui@gmail.com>
2022-01-30 11:24:46 +0100

    port changed

--- mimdb/mysqlconnection.js
+++ mimdb/mysqlconnection.js
@@ -12,7 +12,7 @@ db_con.connect((err) => {
     if (err) {
       console.log("Database Connection Failed !!!", err);
     } else {
-      console.log("connected to Database");
+      console.log("connected to Database .. ");
     }
 });
   
--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 app.use(express.urlencoded({ extended: false }));
-
 
 app.get("/createdb", (req, res) => {
 	let databaseName = "mimidb";
@@ -48,9 +46,9 @@ app.get('/create/tableusers', (req, res) => {
 
 require("./routes")(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-	console.log(`  listening on port ${PORT}.`);
+	console.log(`  listening on port ${PORT}`);
 });
 
 
